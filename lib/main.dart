34341911137e64382aa6a1cdbdee95cd5ebe05ab
sajib <sh.sajibmridha@gmail.com>
2021-06-27 15:43:14 +0600

    change main

--- lib/main.dart
+++ lib/main.dart
@@ -29,7 +29,7 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-          title: Text(""),
+          title: Text("Smart grocery"),
         ),
         body: Center());
   }
