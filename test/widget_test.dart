7e9883de1175500d67040bea9e512b945f8cdc33
sajibnor <sh.sajibmridha@gmail.com>
2021-07-07 20:08:17 +0600

    margi succesful

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:Smart_Grocery/main.dart';
+import 'package:smart_grocery/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
